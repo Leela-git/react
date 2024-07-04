7456c93691594dead1e1d902186ea76d9fdab49d
soniya paneru <lilapaneru910@gmail.com>
2024-07-04 18:27:59 +0545

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000000" fillOpacity="1" d="M0,224L1440,0L1440,320L0,320Z"></path></svg>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/home/Tech.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { DiAndroid, DiCss3, DiHtml5, DiJava, DiJavascript, DiNodejs, DiPython, DiReact } from 'react-icons/di'
+
+const Tech = () => {
+  return (
+    <div className='space-y-5'>
+      <h1 className='text-4xl font-bold text-center'>Technologies I use</h1>
+      <div className="dev-icons  grid grid-cols-4 justify-items-center gap-y-4">
+
+        <DiAndroid size={200} />
+        <DiPython size={200} />
+        <DiCss3 size={200} />
+        <DiReact size={200} className='animate-bounce' />
+        <DiHtml5 size={200} />
+        <DiJava size={200} />
+        <DiJavascript size={200} />
+        <DiNodejs size={200} />
+
+      </div>
+    </div>
+  )
+}
+
+export default Tech
